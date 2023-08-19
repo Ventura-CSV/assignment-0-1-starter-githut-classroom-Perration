3a768a1ecc437d93391171a60a28955aac455287
Perration <yolandag2013@yahoo.com>
2023-08-18 22:10:07 -0700

    first commit

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ def main():
     # make your code below
     # print('Hello World')
     ##############################
-
+    print('Hello, World')
+    print('CS V11: python programming') 
+    
 
 if __name__ == '__main__':
     main()
+ 
+ 
